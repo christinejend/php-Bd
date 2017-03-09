9330655df435470282cd448a4d8f0c4ba7546d78
Christine Jend <christine.jendrzejczyk@gmail.com>
2017-03-09 10:44:00 +0100

    sql reqwest to find one random word

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   $dsn = sprintf('mysql:dbname=%s;host:%s', $cnxDatas['DB_NAME'], $cnxDatas['DB_HOST']);
 
 try{
-  $cnx = new PDO($dsn, $cnxDatas['DB_USER'], $cnxDatas['DB_DB_PASS']);
+  $cnx = new PDO($dsn, $cnxDatas['DB_USER'], $cnxDatas['DB_MDP']);
                       // hostname, mdp
 } catch(PDOExecption $e){
 
@@ -13,5 +13,13 @@ try{
     /* Cette var e est un objet qui va contenir des propriétés et methodes -> qui équivaut au point en js*/
 }
 
+$req = 'SELECT word FROM pendu.words ORDER BY Rand() LIMIT 1 ';
+  $pdoSt = $cnx->query($req); //instance
+          // FETCHALL = Récupération d'un tableau avec tous nos mots
+          // PDO:: (classe) sont des options
+          //FETCH Renvoit directement notre mot
+  $words = $pdoSt->fetch(PDO::FETCH_COLUMN);
+          // FETCH_COLUMN : Convertir le tableau de tableau en tableau de mots
 
-?>
+
+  var_dump($words);
